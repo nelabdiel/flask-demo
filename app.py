f548d5a7927d344670fb6700ca55feba698ed1a2
nelabdiel <nel.abdiel@gmail.com>
2015-11-23 13:15:40 -0500

    testing app file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Started by looking at the source html file from the example app to see how everything was called.
+
 from flask import Flask, render_template, request, redirect
 from bokeh.embed import components
 from bokeh.plotting import figure
@@ -18,11 +20,57 @@ app.vars['color'] = {
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        app.vars['ticker'] = request.form['ticker']
+        app.vars['features'] = request.form.getlist('features')
+
+        if app.vars['ticker'].strip() == '':
+            return redirect('/error-page')
+
+        # Pull stock data
+        url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + app.vars['ticker'] + '/data.json'
+        r = requests.get(url)
+        if r.status_code == 404:
+            return redirect('/error-page')
+        else:
+            data = r.json()['dataset_data']['data']
+            cols = r.json()['dataset_data']['column_names']
+            app.vars['data'] = pd.DataFrame(data, columns=cols)
+            return redirect('/graph')
+
+@app.route('/graph', methods=['GET'])
+def graph():
+    df = app.vars['data']
+
+    p = figure(width=700, height=500, x_axis_type="datetime",
+                title="Data from Quandle WIKI set")
+    for category in app.vars['features']:
+        p.line(pd.to_datetime(df['Date']), df[category],
+                color=app.vars['color'][category], line_width=1,
+                legend=app.vars['ticker'] + ": " + category)
+
+    p.legend.orientation = "top_right"
+
+    plot_resources = RESOURCES.render(
+        js_raw=INLINE.js_raw,
+        css_raw=INLINE.css_raw,
+        js_files=INLINE.js_files,
+        css_files=INLINE.css_files,
+    )
+
+    script, div = components(p, INLINE)
+    html = render_template(
+        'graph.html',
+        ticker=app.vars['ticker'],
+        plot_script=script, plot_div=div, plot_resources=plot_resources
+    )
+    return encode_utf8(html)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
